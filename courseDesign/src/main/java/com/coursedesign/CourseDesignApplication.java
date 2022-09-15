d3434b1834eec24727d47d5b99bf4cd378eddf44
ymy <1619861181@qq.com>
2022-09-15 21:15:34 +0800

    init

--- courseDesign/src/main/java/com/coursedesign/CourseDesignApplication.java
+++ courseDesign/src/main/java/com/coursedesign/CourseDesignApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CourseDesignApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CourseDesignApplication.class, args);
     }
