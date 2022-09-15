57e5821576642d42920fcf4621228c48d004394c
yang fan <279303278@qq.com>
2022-09-15 19:33:02 +0800

    init

---
+++ courseDesign/src/main/java/com/coursedesign/CourseDesignApplication.java
@@ -0,0 +1,13 @@
+package com.coursedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseDesignApplication.class, args);
+    }
+
+}
