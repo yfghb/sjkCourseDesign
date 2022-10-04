d996003b7567ec32d14830c07adce4e22549c631
yang fan <279303278@qq.com>
2022-10-04 19:34:29 +0800

    -杨帆 去掉了mp重复的分页配置

--- courseDesign/src/main/java/com/coursedesign/CourseDesignApplication.java
+++ courseDesign/src/main/java/com/coursedesign/CourseDesignApplication.java
@@ -2,8 +2,10 @@ package com.coursedesign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class CourseDesignApplication {
     public static void main(String[] args) {
         SpringApplication.run(CourseDesignApplication.class, args);
