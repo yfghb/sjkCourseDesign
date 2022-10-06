680356452d289fda1b7fe3df53bcbdb294ef51c9
yang fan <279303278@qq.com>
2022-10-06 19:38:03 +0800

    -杨帆  对于user表的增删改查功能都完成了

--- courseDesign/src/test/java/com/coursedesign/service/UserServiceTest.java
+++ courseDesign/src/test/java/com/coursedesign/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.coursedesign.service;
 
+import com.coursedesign.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,5 +11,19 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @Test
+    public void getByAccount(){
+        System.out.println(userService.getByAccount("administer"));
+    }
 
+    @Test
+    public void createUser(){
+        User u = new User();
+        u.setAccount("test");
+        u.setPassword("123456");
+
+        if(userService.save(u)){
+            System.out.println("yes!");
+        }
+    }
 }
